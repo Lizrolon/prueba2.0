c2b00857d0d6be39809ebdcd8fdddba5617ad817
Lizrolon <lizroloncarnichi@gmail.com>
2023-08-09 22:04:38 -0300

    Initial commit

---
+++ src/prueba2/pkg0/Prueba20.java
@@ -0,0 +1,17 @@
+
+package prueba2.pkg0;
+
+/**
+ *
+ * @author Michi
+ */
+public class Prueba20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
